5be884b12138f75a73b3fe98c14fcb99e072cf78
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2025-03-30 10:21:39 +0800

    chore(`macro_rules_attr`): Update doc; Code refactor

--- macro_rules_attr/src/lib.rs
+++ macro_rules_attr/src/lib.rs
@@ -10,13 +10,12 @@ use proc_macro::{Delimiter, Group, Punct, Spacing, TokenStream, TokenTree};
 
 /// Emit a compile error with the given message
 macro_rules! error {
-    ($msg:expr) => {{
-        let ts: ::proc_macro::TokenStream = ::core::stringify! {
+    ($msg:literal) => {{
+        ::core::stringify! {
             ::core::compile_error!($msg)
         }
         .parse()
-        .unwrap();
-        ts
+        .unwrap()
     }};
 }
 
@@ -28,9 +27,8 @@ pub fn apply(attrs: TokenStream, input: TokenStream) -> TokenStream {
     // Parse `the_macro` and `macro_append` from `attrs`
     let mut tts = attrs.into_iter();
 
-    let macro_name = match tts.next() {
-        Some(tt) => tt,
-        None => return error!("Expected an identifier to `apply`, found nothing"),
+    let Some(macro_name) = tts.next() else {
+        return error!("Expected an identifier to `apply`, found nothing");
     };
     if !matches!(macro_name, TokenTree::Ident(_)) {
         return error!("Expected an identifier to `apply`, found something else");
@@ -53,19 +51,12 @@ fn macro_rules_attr_impl(
     input: TokenStream,
 ) -> TokenStream {
     // Adapted from https://github.com/danielhenrymantilla/macro_rules_attribute-rs/blob/fa6f120939c7757dec23589e2687d5e8480fa1ce/src/proc_macro/mod.rs#L27-L53
-
-    let mut ret = macro_name;
     let mut macro_input = input;
-    ret.extend([TokenTree::Punct(Punct::new('!', Spacing::Alone))]);
+    let mut ret = macro_name;
 
-    // Append `macro_append` to `macro_input` (With delimiter `,`)
-    if !macro_append.is_empty() {
-        // Append `macro_append`
-        macro_input.extend(macro_append);
-    }
-
-    // Wrap with `{}`
-    ret.extend([TokenTree::Group(Group::new(Delimiter::Brace, macro_input))]);
+    macro_input.extend(macro_append); // Append `macro_append` to `macro_input`
+    ret.extend([TokenTree::Punct(Punct::new('!', Spacing::Alone))]); // Append `!` to invoke the macro
+    ret.extend([TokenTree::Group(Group::new(Delimiter::Brace, macro_input))]); // Wrap with `{}`
 
     #[cfg(feature = "log")]
     debug!("ret: {ret:?}");
